d47b6d7c35bf1e6749c4ab3cb9f027d4d5ab0266
Richard Frost <frost.rchrd@gmail.com>
2019-01-10 15:59:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 function printBadges(emplayees) {
-  for  (let i = 0; i < employees.length)
+  for  (let i = 0; i < employees.length; i++)
+{console.log('Welcome ${employees[i] you are employee # ${i + 1}.')}
   
 }
