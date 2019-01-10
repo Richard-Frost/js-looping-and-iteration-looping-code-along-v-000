806e74ae3d93ec9424481ab50d2cfed8efe0281c
Richard Frost <frost.rchrd@gmail.com>
2019-01-10 16:14:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 const employees = ['Ada', 'Brendan', 'Ali']
 
 function printBadges(emplayees) {
-  
-  for  (let i = 0; i < employees.length; i++)
-{
-  console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`)
+  for  (let i = 0; i < employees.length; i++){
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
   
 }
 return employees
   
 }
+
