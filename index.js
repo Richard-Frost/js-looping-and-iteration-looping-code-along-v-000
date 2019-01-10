47fbc5dacbc55f404c7ac93e742d2fe45ca0b379
Richard Frost <frost.rchrd@gmail.com>
2019-01-10 16:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solutions in this file
+const employees = ['Ada', 'Brendan', 'Ali']
+
 function printBadges(emplayees) {
-  const emplayees = [...employees]
+  
   for  (let i = 0; i < employees.length; i++)
 {console.log('Welcome ${employees[i] you are employee # ${i + 1}.')}
 return employees
